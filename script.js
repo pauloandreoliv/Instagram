45401308fb47defe1ac005066a3948e0a1c29983
Paulo André <pauloandrejanga@gmail.com>
2023-03-13 15:19:00 -0300

    Inserindo JS

---
+++ script.js
@@ -0,0 +1,13 @@
+function verReels() {
+  document.getElementById("publicacoes").style.display = "none";
+  document.getElementById("item_publicacoes").style.cssText="border: none; font-weight: 0;";
+  document.getElementById("item_reels").style.cssText="border-top: 1px solid black; font-weight: bold;";
+  document.getElementById("reels").style.display = "block";
+}
+
+function verPublicacoes() {
+  document.getElementById("publicacoes").style.display = "block";
+  document.getElementById("item_publicacoes").style.cssText="border-top: 1px solid black; font-weight: bold;";
+  document.getElementById("item_reels").style.cssText="border-top: none; font-weight: 0;";
+  document.getElementById("reels").style.display = "none";
+}
