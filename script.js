82c4293e51b96f3ea4ca755f4198aa1f03728ec1
Paulo André <pauloandrejanga@gmail.com>
2023-03-13 15:50:05 -0300

    Inserindo reels

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ function verReels() {
 
 function verPublicacoes() {
   document.getElementById("publicacoes").style.display = "block";
-  document.getElementById("item_publicacoes").style.cssText="border-top: 1px solid black; font-weight: bold;";
   document.getElementById("item_reels").style.cssText="border-top: none; font-weight: 0;";
+  document.getElementById("item_publicacoes").style.cssText="border-top: 1px solid black; font-weight: bold;";
   document.getElementById("reels").style.display = "none";
 }
